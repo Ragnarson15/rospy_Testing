a2c7ffb7e68cd8fce3bd3c7a390d2d960e33a9aa
Andrew Hand <33326997+Ragnarson15@users.noreply.github.com>
2020-06-03 18:05:31 -0400

    Add files via upload

--- src/pyService.py
+++ src/pyService.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 import rospy
 
+rospy.init_node('test_service_call')
 rospy.wait_for_service('/qual_2_services/volatile_locations')
 qual_2_volitiles = rospy.ServiceProxy('/qual_2_services/volatile_locations', qual_2_volatiles_srv)
 responce = qual_2_volitiles()
 print (response)
+rospy.spin()
